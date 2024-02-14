13dd4221ac9e01a778516a9ff3086a96390d5347
Ravi Local <ravilocal@mbp.local>
2024-02-13 22:26:12 -0500

    CORS added v1

--- client/src/controller/urlObj.js
+++ client/src/controller/urlObj.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://omissions-sample-pine-specific.trycloudflare.com/dashboard';
+export const API_URL = 'https://omissions-sample-pine-specific.trycloudflare.com/millenials';
